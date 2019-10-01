5d35176b0b7c34b7154e4b96d6ca10aef0a9c7df
thanhle <mix2pigcat@gmaail.com>
2019-10-01 18:42:47 +0700

    filebai6

---
+++ javascript.js
@@ -0,0 +1,12 @@
+let a=parseInt(prompt('Khach hang 1:'));
+let b=parseInt(prompt('Khach hang 2:'));
+let c=parseInt(prompt('Khach hang 3'));
+result=(a+b+c);
+
+if(result>20){
+    alert("tien hoa hong la 7tr");
+}else if (result>15){
+    alert("tien hoa hong la 5tr");
+}else{
+    alert("tien hoa hong la 2tr");
+}
